d42beecaaa9443d405de9fc17a32d658a50940cc
Jasmine Marwaha <marwahaj@uci.edu>
2025-03-14 07:07:20 +0000

    fix dijkastras hopefully

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,52 +1,60 @@
 #include "dijkstras.h"
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-    // Initialize distances, previous nodes, and visited flags
-    vector<int> distances(G.numVertices, INF);
-    previous.resize(G.numVertices, -1);
-    vector<bool> visited(G.numVertices, false);
+    // vector<int> distances(G.numVertices, INF);
+    // previous.resize(G.numVertices, -1);
+    // vector<bool> visited(G.numVertices, false);
     
-    // Set distance to source as 0
+    // distances[source] = 0;
+    // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    
+    // pq.push({0, source});
+    
+    // while (!pq.empty()) {
+    //     int u = pq.top().second;
+    //     pq.pop();
+        
+    //     if (visited[u]) {
+    //         continue;
+    //     }
+        
+    //     visited[u] = true;
+        
+    //     for (const Edge& edge : G[u]) {
+    //         int v = edge.dst;
+    //         int weight = edge.weight;
+            
+    //         if (!visited[v] && distances[u] != INF && distances[u] + weight < distances[v]) {
+    //             distances[v] = distances[u] + weight;
+    //             previous[v] = u;
+    //             pq.push({distances[v], v});
+    //         }
+    //     }
+    // }
+    
+    // return distances;
+    vector<int> distances(G.size(), INF);
     distances[source] = 0;
     
-    // Priority queue for Dijkstra's algorithm
-    // Using pair<int, int> where first is distance and second is vertex
-    // We use min heap based on distances
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    using pii = pair<int, int>;
+    priority_queue<pii, vector<pii>, greater<pii>> queue;
+    queue.push({0, source});
     
-    // Push source vertex with distance 0
-    pq.push({0, source});
+    previous = vector<int>(G.size(), -1);
     
-    while (!pq.empty()) {
-        // Get vertex with minimum distance
-        int u = pq.top().second;
-        pq.pop();
+    while (!queue.empty()) {
+        int distance = queue.top().first;
+        int vertex = queue.top().second;
+        queue.pop();
         
-        // If already visited, skip
-        if (visited[u]) {
-            continue;
+        for (auto& e : G[vertex]) {
+        if (distances[vertex] + e.weight < distances[e.dst]) {
+            distances[e.dst] = distances[vertex] + e.weight;
+            queue.push({distances[e.dst], e.dst});
+            previous[e.dst] = vertex;
         }
-        
-        // Mark as visited
-        visited[u] = true;
-        
-        // Check all neighbors of u
-        for (const Edge& edge : G[u]) {
-            int v = edge.dst;
-            int weight = edge.weight;
-            
-            // If there's a shorter path to v through u
-            if (!visited[v] && distances[u] != INF && distances[u] + weight < distances[v]) {
-                // Update distance
-                distances[v] = distances[u] + weight;
-                // Update previous node
-                previous[v] = u;
-                // Add to priority queue
-                pq.push({distances[v], v});
-            }
         }
     }
-    
     return distances;
 }
 
@@ -56,13 +64,11 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
     //     return {};
     // }
     
-    // Reconstruct path using previous array
     vector<int> path;
     for (int v = destination; v != -1; v = previous[v]) {
         path.push_back(v);
     }
     
-    // Reverse to get path from source to destination
     reverse(path.begin(), path.end());
     
     return path;
