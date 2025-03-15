f79cc382ce96202ff53f07077f30b3e253688615
Jasmine Marwaha <marwahaj@uci.edu>
2025-03-15 06:54:10 +0000

    more commits

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -24,6 +24,8 @@ using namespace std;
 //     return distances;
 // }
 
+//pseudocode on hw 
+
 struct Node {
     int vertex;
     int weight;
@@ -89,10 +91,10 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
 }
 
 void print_path(const vector<int>& v, int total) {
-    if (v.empty()) { //todo: do not know if this is needed
-        cout << "\nTotal cost is " << total << endl;
-        return;
-    }
+    // if (v.empty()) { //todo: do not know if this is needed
+    //     cout << "\nTotal cost is " << total << endl;
+    //     return;
+    // }
     for (size_t i = 0; i < v.size(); ++i) {
         cout << v[i];
         if (i < v.size() - 1) {
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -5,15 +5,15 @@ int main() {
     load_words(word_list, "src/words.txt");
     
     string begin_word, end_word;
-    cout << "Enter start word: ";
+    cout << "Enter the start word: ";
     cin >> begin_word;
     
-    cout << "Enter end word: ";
+    cout << "Enter the end word: ";
     cin >> end_word;
     
     if (begin_word == end_word) {
         error(begin_word, end_word, "Start and end words cannot be the same");
-        return 1;
+        //return 1;
     }
     
     vector<string> ladder = generate_word_ladder(begin_word, end_word, word_list);
